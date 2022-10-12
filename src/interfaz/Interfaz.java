dbb25fa3ca8ab53e18081cba47c6d176c1aef005
Gastón <gaelceart@gmail.com>
2022-10-12 19:49:01 -0300

    initial commit

---
+++ src/interfaz/Interfaz.java
@@ -0,0 +1,71 @@
+package interfaz;
+
+import java.awt.EventQueue;
+
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+import java.awt.Cursor;
+import java.awt.event.MouseMotionAdapter;
+
+@SuppressWarnings("rawtypes")
+public class Interfaz {
+
+	private JFrame frame;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interfaz window = new Interfaz();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Interfaz() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		setupFrame();
+	}
+	private void setupFrame() {
+		frame = Recurso.setupFrame();
+
+		try {
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void updateFrame() {
+		SwingUtilities.updateComponentTreeUI(frame);
+		frame.setVisible(true);
+		frame.toFront();
+		frame.requestFocus();
+	}
+}
---
+++ src/interfaz/Recurso.java
@@ -0,0 +1,19 @@
+package interfaz;
+
+import javax.swing.JFrame;
+
+public class Recurso {
+
+	 public static JFrame setupFrame() {
+		JFrame frame = new JFrame();
+		frame.setResizable(false);
+		frame.setTitle("Simulador Album Mundial");
+		//Toolkit miPantalla = Toolkit.getDefaultToolkit();
+		//Image miIcono = miPantalla.getImage("src/icono.png");
+		//frame.setIconImage(miIcono);
+		frame.setBounds(0, 0, 800, 600);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		return frame;
+	 }
+}
