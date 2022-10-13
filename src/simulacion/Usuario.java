c066883246687d678c454fc1cc147b9749f10b2b
Leandr0Fr <leandro00fr@gmail.com>
2022-10-13 03:24:45 -0300

    ADD variables Usuario

--- src/simulacion/Usuario.java
+++ src/simulacion/Usuario.java
@@ -1,5 +1,14 @@
 package simulacion;
 
-public class Usuario {
+import java.util.List;
+import albumMundial.*;
 
+public class Usuario {
+	private Album _album;
+	private List<Figurita> _figuritasRepetidas;
+	private int _paquetesComprados;
+	
+	public Usuario() {
+		
+	}
 }
