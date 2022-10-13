88eeeb04ba909284a0c4a2207710e2ee7efa70a2
Leandr0Fr <leandro00fr@gmail.com>
2022-10-13 03:00:43 -0300

    ADD method crearPaquete

--- src/albumMundial/Figurita.java
+++ src/albumMundial/Figurita.java
@@ -3,16 +3,16 @@ package albumMundial;
 public class Figurita {
 	private int _numFigurita;
 	private boolean _pocoComun;
-	
+
 	public Figurita(int numero, boolean pocoComun) {
 		_numFigurita = numero;
 		_pocoComun = pocoComun;
 	}
-	
+
 	public int getNumero() {
 		return _numFigurita;
 	}
-	
+
 	public boolean isPocoComun() {
 		return _pocoComun;
 	}
--- src/albumMundial/Paquete.java
+++ src/albumMundial/Paquete.java
@@ -1,8 +1,35 @@
 package albumMundial;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Paquete {
 	private int _Cantidadfiguritas;
 	private List<Figurita> _figuritas;
+
+	public Paquete(int figuritasPorPaquete) {
+		if (figuritasPorPaquete <= 0) {
+			throw new IllegalArgumentException(
+					"Paquete debe contener mas de 0 figuritas por paquete:" + figuritasPorPaquete);
+		}
+		_Cantidadfiguritas = figuritasPorPaquete;
+		_figuritas = new ArrayList<>();
+		crearPaquete();
+	}
+
+	public void crearPaquete() {
+		while (tamanoPaquete() < _Cantidadfiguritas) {
+			Random random = null;
+			int figuritaSeleccionada = random.nextInt(638);
+			Figurita figurita = new Figurita(figuritaSeleccionada, false);
+			if (!_figuritas.contains(figurita)) {
+				_figuritas.add(figurita);
+			}
+		}
+	}
+
+	public int tamanoPaquete() {
+		return _figuritas.size();
+	}
 }
