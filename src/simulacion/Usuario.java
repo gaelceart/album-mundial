098f35545b1f9d855b780e48ba56d5fc65d64ddd
Gastón <gaelceart@gmail.com>
2022-10-13 15:55:03 -0300

    ADD Usuario methods

--- src/simulacion/Usuario.java
+++ src/simulacion/Usuario.java
@@ -1,5 +1,6 @@
 package simulacion;
 
+import java.util.LinkedList;
 import java.util.List;
 import albumMundial.*;
 
@@ -9,27 +10,62 @@ public class Usuario {
 	private int _paquetesComprados;
 	
 	public Usuario() {
-		
+		_album = new Album(638);
+		_figuritasRepetidas = new LinkedList<>();
+		_paquetesComprados = 0;
 	}
 	public static void main(String[] args) {
-		Album album = new Album(638);
-		int paquetesComprados = 0;
+		Usuario[] users = new Usuario[2];
+		for (int i = 0; i < users.length; i++)
+			users[i] = new Usuario();
+		
+		int paquetesTotalesComprados = 0;
 		int figusRepetidas = 0;
-		while (!album.isCompleto()) {
-			Paquete paquete = new Paquete(5);
+		while (!users[0].esAlbumCompleto()) {
+			Paquete paquete = users[0].comprarPaquete(5);
 			System.out.println(paquete);
+			paquetesTotalesComprados++;
 			for (Figurita f : paquete.getFiguritas()) {
-				if (album.figuritaPegada(f)) {
+				if (users[0].esFiguritaRepetida(f)) {
 					figusRepetidas++;
 				}
 				else {
-					album.pegarFigurita(f);
+					users[0].pegarFigurita(f);
 				}
 			}
-			paquetesComprados++;
 		}
-		System.out.println(album.getFiguritasEncontradas().size());
-		System.out.println(paquetesComprados);
+		//System.out.println(album.getFiguritasEncontradas().size());
+		System.out.println(paquetesTotalesComprados);
 		System.out.println(figusRepetidas);
+		System.out.println(users[0].getFiguritasRepetidas().size());
 	}
+	
+	public Paquete comprarPaquete(int cantFigus) {
+		_paquetesComprados++;
+		return new Paquete(cantFigus);
+	}
+	
+	public boolean esFiguritaRepetida(Figurita f) {
+		if (!_album.esFiguritaRepetida(f))
+			return false;
+		_figuritasRepetidas.add(f);
+		return true;
+	}
+
+	public void pegarFigurita(Figurita f) {
+		_album.pegarFigurita(f);
+	}
+	
+	public boolean esAlbumCompleto() {
+		return _album.isCompleto();
+	}
+	
+	public int getCantidadPaquetesComprados() {
+		return _paquetesComprados;
+	}
+	
+	public List<Figurita> getFiguritasRepetidas() {
+		return _figuritasRepetidas;
+	}
+	
 }
