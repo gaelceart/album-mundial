93105fb2559c4526769846c32226ee52626ec043
Leandr0Fr <leandro00fr@gmail.com>
2022-10-13 03:10:54 -0300

    ADD constructor Album

--- src/albumMundial/Album.java
+++ src/albumMundial/Album.java
@@ -1,5 +1,17 @@
 package albumMundial;
 
-public class Album {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Album {
+	private int _cantidadFiguritas;
+	private Map<Integer, Boolean> _figuritasEncontradas;
+	
+	public Album(int cantidadFiguritas) {
+		if (cantidadFiguritas <= 0) {
+			throw new IllegalArgumentException("Un album no puede contener menos de 0 figuritas: " + cantidadFiguritas);
+		}
+		_cantidadFiguritas = cantidadFiguritas;
+		_figuritasEncontradas = new HashMap<>();
+	}
 }
