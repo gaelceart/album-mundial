f252383a9385cdb85023a2ee88845981ee1d754b
Gastón <gaelceart@gmail.com>
2022-10-13 17:54:09 -0300

    REMOVE Paquete.iterator() -> USES _figuritas.iterator()

--- src/albumMundial/Paquete.java
+++ src/albumMundial/Paquete.java
@@ -43,45 +43,13 @@ public class Paquete {
 	public String toString() {
 		return "Paquete [_cantidadFiguritas=" + _cantidadFiguritas + ", _figuritas=" + _figuritas + "]\n";
 	}
-	
-    public Iterator<Figurita> iterator() {
-		Iterator<Figurita> it = new Iterator<Figurita>() {
-			boolean canRemove = false;
-		    int previousLoc = -1;
-
-			private int currentIndex = 0;
-
-			@Override
-			public boolean hasNext() {
-				return currentIndex < _currentSize;
-			}
-
-			@Override
-			public Figurita next() {
-				previousLoc++;
-				canRemove = true;
-				return _figuritas.get(currentIndex++);
-			}
-
-			@Override
-			public void remove() {
-				if (!canRemove)
-					throw new IllegalStateException();
-				_figuritas.remove(previousLoc);
-				canRemove = false;
-				_currentSize--;
-			}
-			
-		};
-		return it;
-	}
     
     public static void main(String[] args) {
     	Album a = new Album(638);
 		Paquete p = new Paquete(3);
 		System.out.println(p.toString());
 		
-		Iterator<Figurita> it = p.iterator();
+		Iterator<Figurita> it = p.getFiguritas().iterator();
 		while (it.hasNext()) {
 			Figurita f = it.next();
 			if (f.getNumero() % 2 == 0) {
