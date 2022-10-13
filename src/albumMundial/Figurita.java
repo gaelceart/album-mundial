dcfe5d4b65f93f753e6d49631ec30330678f8ad3
Leandr0Fr <leandro00fr@gmail.com>
2022-10-13 02:44:17 -0300

    CREATE constructor Figurita

--- src/albumMundial/Figurita.java
+++ src/albumMundial/Figurita.java
@@ -1,5 +1,12 @@
 package albumMundial;
 
 public class Figurita {
-
+	private int _numFigurita;
+	private boolean _pocoComun;
+	
+	public Figurita(int numero, boolean pocoComun) {
+		_numFigurita = numero;
+		_pocoComun = pocoComun;
+	}
+	
 }
