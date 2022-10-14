a84b0248aabc5546a36eb8d5d6a3187a6ac88804
Gastón <gaelceart@gmail.com>
2022-10-14 00:59:17 -0300

    ADD enum tipoDeEscenario

--- src/simulacion/Simulacion.java
+++ src/simulacion/Simulacion.java
@@ -13,14 +13,18 @@ public class Simulacion implements Runnable {
 	private	int _paquetesTotalesComprados;
 	private	int _figusTotalesRepetidas;
 	private int _cantidadFigusAlbum;
+	private enum tipoEscenario{individual, donacion, intercambio};
+	private tipoEscenario _escenario;
 
-	public Simulacion(int cantUsuarios, int cantFigusAlbum, int cantFigusPorPaquete) {
+	public Simulacion(int cantUsuarios, int cantFigusAlbum, int cantFigusPorPaquete, tipoEscenario e) {
 		_cantidadFigusAlbum = cantFigusAlbum;
 		_cantidadFigusPorPaquete = cantFigusPorPaquete;
 		_users = inicializarUsers(cantUsuarios);
 		_paquetesTotalesComprados = 0;
 		_figusTotalesRepetidas = 0;
+		_escenario = e;
 	}
+
 	
 	private Usuario[] inicializarUsers(int cantUsuarios) {
 		Usuario[] ret = new Usuario[cantUsuarios];
@@ -30,25 +34,27 @@ public class Simulacion implements Runnable {
 	}
 
 	public static void main(String[] args) throws InterruptedException {
-		Simulacion s = new Simulacion(15000, 638, 5);
+		Simulacion s = new Simulacion(2, 638, 5, tipoEscenario.individual);
 		Thread t1 = new Thread(s);
-		Simulacion s2 = new Simulacion(15000, 638, 5);
+		t1.start();
+		System.out.println("CARGANDO...\n");
+		t1.join();
+		/*
+		Simulacion s2 = new Simulacion(15000, 638, 5, tipoEscenario.individual);
 		Thread t2 = new Thread(s2);
-		Simulacion s3 = new Simulacion(15000, 638, 5);
+		Simulacion s3 = new Simulacion(15000, 638, 5, tipoEscenario.individual);
 		Thread t3 = new Thread(s3);
-		Simulacion s4 = new Simulacion(15000, 638, 5);
+		Simulacion s4 = new Simulacion(15000, 638, 5, tipoEscenario.individual);
 		Thread t4 = new Thread(s4);
 		
-		t1.start();
 		t2.start();
 		t3.start();
 		t4.start();
-		System.out.println("CARGANDO...\n");
 		
-		t1.join();
 		t2.join();
 		t3.join();
 		t4.join();
+		*/
 	}
 	
 	@Override
@@ -62,8 +68,15 @@ public class Simulacion implements Runnable {
 				if (paquetes.containsKey(i))
 					_users[i].pegarFiguritas(paquetes.get(i));
 			
+			//Fase 3 Donar
+			if (_escenario == tipoEscenario.donacion) {
+				//donar
+			}
 			
 			//Fase 3 Intercambiar -NO IMPLEMENTADO
+			if (_escenario == tipoEscenario.intercambio) {
+				//intercambio
+			}
 		
 			//Fase 4 Volver a pegar figuritas si hubo intercambio
 			
