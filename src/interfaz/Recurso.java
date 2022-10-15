33b5f6add47a988326bc7292a75ddfef4cb2a7d8
locomaldelcoco <santiicarp1@gmail.com>
2022-10-15 18:34:51 -0300

    add icono.png

--- src/interfaz/Recurso.java
+++ src/interfaz/Recurso.java
@@ -1,5 +1,8 @@
 package interfaz;
 
+import java.awt.Image;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Recurso {
@@ -8,12 +11,16 @@ public class Recurso {
 		JFrame frame = new JFrame();
 		frame.setResizable(false);
 		frame.setTitle("Simulador Album Mundial");
-		//Toolkit miPantalla = Toolkit.getDefaultToolkit();
-		//Image miIcono = miPantalla.getImage("src/icono.png");
-		//frame.setIconImage(miIcono);
+		Toolkit miPantalla = Toolkit.getDefaultToolkit();
+		Image miIcono = miPantalla.getImage("src/icono.png");
+		frame.setIconImage(miIcono);
 		frame.setBounds(0, 0, 800, 600);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		return frame;
+	 }
+	 
+	 public static Jpanel setupAlbumContainer{
+		 
 	 }
 }
