cb2377e1b80596e10b9a71bc6463defb21e03630
Gastón <gaelceart@gmail.com>
2022-10-16 01:38:12 -0300

    CHANGE paquete

--- src/albumMundial/Paquete.java
+++ src/albumMundial/Paquete.java
@@ -1,63 +1,20 @@
 package albumMundial;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
 public class Paquete {
-	private int _cantidadFiguritas;
-	private List<Figurita> _figuritas;
 
-	public Paquete(int figuritasPorPaquete) {
-		if (figuritasPorPaquete <= 0) 
-			throw new IllegalArgumentException(
-					"Un Paquete no puede contener tener 0 o menos figuritas:" + figuritasPorPaquete);
-		
-		if (figuritasPorPaquete > Album._cantidadFiguritas)
-			throw new IllegalArgumentException("Un paquete no puede contener mas figuritas que el album: Cant. figus paquete = " + figuritasPorPaquete + " > Cant. figus album = " + Album._cantidadFiguritas);
-		
-		_cantidadFiguritas = figuritasPorPaquete;
-		_figuritas = new ArrayList<>();
-		crearPaquete();
-	}
-
-	// crearPaquete(int figuritasPosibles)? o genera acomplamiento
-	public void crearPaquete() {
-		while (tamanoPaquete() < _cantidadFiguritas) {
-			Figurita figurita = generarFigurita();
-			agregarFigurita(figurita);
-		}
-	}
-
-	private Figurita generarFigurita() {
+	public static Integer[] comprarPaquete(int cantFigus) {
+		List<Integer> paquete = new ArrayList<>();
 		Random random = new Random();
-		int figuritaSeleccionada = random.nextInt(Album._cantidadFiguritas);
-		Figurita figurita = new Figurita(figuritaSeleccionada, false);
-		return figurita;
+		while (paquete.size() < cantFigus) {
+			int figuritaSeleccionada = random.nextInt(Album._cantidadFiguritas);
+			if (!paquete.contains(figuritaSeleccionada)) 
+				paquete.add(figuritaSeleccionada);
+			
+		}
+		return paquete.toArray(new Integer[cantFigus]);
 	}
-	
-	private void agregarFigurita(Figurita f) {
-			if (tamanoPaquete() == 0)
-				_figuritas.add(f);
-
-			for (int i = 0; i < tamanoPaquete(); i++) {
-				if (!f.equals(_figuritas.get(i)))
-					_figuritas.add(f);
-			}
-	}
-
-	public List<Figurita> abrirPaquete() {
-		return _figuritas;
-	}
-
-	public int tamanoPaquete() {
-		return _figuritas.size();
-	}
-
-	@Override
-	public String toString() {
-		return "Paquete [_cantidadFiguritas=" + _cantidadFiguritas + ", _figuritas=" + _figuritas + "]\n";
-	}
-    
 }
