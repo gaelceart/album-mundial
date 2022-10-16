ecc771c8373813472d603ac376c43588f87b5567
Gastón <gaelceart@gmail.com>
2022-10-15 23:01:57 -0300

    ADD var _cantidadFigusRepetidasTotal

--- src/simulacion/Usuario.java
+++ src/simulacion/Usuario.java
@@ -9,11 +9,13 @@ public class Usuario {
 	private Album _album;
 	private int _paquetesComprados;
 	private ArrayList<Integer> _figuritasRepetidas;
+	private int _cantidadFigusRepetidasTotal;
 	
 	public Usuario(int cantFigusAlbum) {
 		_album = new Album(cantFigusAlbum);
 		_figuritasRepetidas = new ArrayList<>();
 		_paquetesComprados = 0;
+		_cantidadFigusRepetidasTotal = 0;
 	}
 
 	public Integer[] comprarPaquete(int cantFigus) {
@@ -45,11 +47,16 @@ public class Usuario {
 		for (int i = 0; i < paquete.length; i++) {
 			if (_album.esFiguritaRepetida(paquete[i])) {
 				_figuritasRepetidas.add(paquete[i]);
+				_cantidadFigusRepetidasTotal = getCantidadFigusRepetidasTotal() + 1;
 			}
 			else {
 				pegarFigurita(paquete[i]);
 			}
 		}
+	}
+
+	public int getCantidadFigusRepetidasTotal() {
+		return _cantidadFigusRepetidasTotal;
 	}
 	
 }
