7d8fec70dcc661589ffc864dd5191293aa78a232
Gastón <gaelceart@gmail.com>
2022-10-18 22:18:25 -0300

    UPDATE Model

--- src/simulacion/Model.java
+++ src/simulacion/Model.java
@@ -13,8 +13,22 @@ public class Model {
 	private int _cantFigusAlbum;
 	private int _cantFigusPaquete;
 	private double _costoPaquete;
-	tipoEscenario _escenario = tipoEscenario.donacion;
+	private double _costoTotal;
+	tipoEscenario _escenario;
+
+	private Simulacion[] _s;
+	private Thread[] _t;
 	
+	public Model() {
+		_cantSimulaciones = 0;
+		_cantUsuarios = 0;
+		_cantFigusAlbum = 0;
+		_cantFigusPaquete = 0;
+		_costoPaquete = 0;
+		_costoTotal = 0;
+		_escenario = tipoEscenario.donacion;
+	}
+
 	public void setCantSimulaciones(int n) {
 		_cantSimulaciones = n;
 	}
@@ -39,36 +53,66 @@ public class Model {
 		_escenario = e;
 	}
 	
-	double main() throws InterruptedException {
-		Album.crearGeneradorAleatorio();
-		Paquete.crearGeneradorAleatorio();
-
-		double costoTotal = 0.0;
-		double costoPromedio = 0.0;
-
-		Simulacion[] s = new Simulacion[_cantSimulaciones];
-		Thread[] t = new Thread[_cantSimulaciones];
-
-		for (int i = 0; i < _cantSimulaciones; i++) {
-			s[i] = new Simulacion(_cantUsuarios, _cantFigusAlbum, _cantFigusPaquete, _escenario);
-			t[i] = new Thread(s[i]);
-			t[i].start();
-		}
+	double simular() {
+		
+		initSimulaciones();
+		initThreads();
+		startThreads();
+		stopThreads();
+		calcCostoTotal();
 
 		System.out.println("CARGANDO...\n");
 
-		for (int i = 0; i < _cantSimulaciones; i++) {
-			t[i].join();
-			costoTotal += s[i].getPaquetesTotalesComprados() * _costoPaquete / _cantUsuarios;
-		}
-		costoPromedio = costoTotal / _cantSimulaciones;
-		System.out.println("COSTO TOTAL: " + costoTotal);
+		double costoPromedio = _costoTotal / _cantSimulaciones;
+		System.out.println("COSTO TOTAL: " + _costoTotal);
 		System.out.println("COSTO PROMEDIO: " + costoPromedio);
 		
 		return costoPromedio;
 	}
 
+	private void initSimulaciones() {
+		_s = new Simulacion[_cantSimulaciones];
+		for (int i = 0; i < _cantSimulaciones; i++) 
+			_s[i] = new Simulacion(_cantUsuarios, _cantFigusAlbum, _cantFigusPaquete, _escenario);
+	}
+
+	private void initThreads() {
+		_t = new Thread[_cantSimulaciones];
+		for (int i = 0; i < _cantSimulaciones; i++)
+			_t[i] = new Thread(_s[i]);
+	}
+
+	private void startThreads() {
+		for (int i = 0; i < _cantSimulaciones; i++)
+			_t[i].start();
+	}
+	
+	private void stopThreads() {
+		for (int i = 0; i < _cantSimulaciones; i++) {
+			tryJoinThread(i);
+		}
+	}
+
+	private void tryJoinThread(int i) {
+		try {
+			_t[i].join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void calcCostoTotal() {
+		for (int i = 0; i < _cantSimulaciones; i++)
+			sumarCostoDeSimulacion(i);
+	}
+
+	private void sumarCostoDeSimulacion(int i) {
+		_costoTotal += _s[i].getPaquetesTotalesComprados() * _costoPaquete / _cantUsuarios;
+	}
+
 	public void iniciarSimulacion() {
-		
+		Album.crearGeneradorAleatorio();
+		Paquete.crearGeneradorAleatorio();
+		double costoPromedio = simular();
 	}
 }
