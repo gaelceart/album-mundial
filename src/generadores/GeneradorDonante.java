c147b101ede54c2e7e73c3839ab89e694e5ba8db
Leandr0Fr <leandro00fr@gmail.com>
2022-10-26 22:34:50 -0300

    FIX GeneradorDonante

--- src/generadores/GeneradorDonante.java
+++ src/generadores/GeneradorDonante.java
@@ -1,32 +1,11 @@
 package generadores;
 
 public class GeneradorDonante implements Generador {
-	int[] figusUsuario1 = { 0, 1, 2, 3, 2, 3, 4, 5, 4, 5, 6, 7, 6, 7, 8, 9};
+	int[] n = {0,1,0,1,2,3,0,1,2,3,0,1,4,5,0,1,4,5,0,1,6,7,0,1,6,7,0,1,8,9,8,9};
 	int puntero = 0;
-	int figusEnPaquete = 0;
-	int usuario = 1;
-	int aux = 8;
 	@Override
 	public int nextInt(int rango) {
-		if (puntero >=16) {
-			return aux++;
-		}
-		if (usuario == 1) {
-			if (figusEnPaquete == 2) {
-				figusEnPaquete = 0;
-				usuario = 2;
-				System.out.println("-");
-				System.out.println(0);
-				return 0;
-			}
-			System.out.println(figusUsuario1[puntero]);
-			++figusEnPaquete;
-			return figusUsuario1[puntero++];
-		}
-		usuario = 1;
-		System.out.println(1);
-		System.out.println("-");
-		return 1;
+		return n[puntero++];
 	}
 
 	@Override
